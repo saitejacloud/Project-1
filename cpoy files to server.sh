3f0c7df08e29fbfc8fea4c4472db8c248a2b95c2
Nani <saiteja.cloud4@gmail.com>
2018-03-24 14:03:34 -0700

    Delete cpoy files to server.sh

--- cpoy files to server.sh
+++
@@ -1,5 +0,0 @@
-scp -i keys/keys/merdep.non-prod -o PreferredAuthentications=publickey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no PATH/FILE.py $USERNAME@$HOSTNAME:FILE.py
-ssh -i keys/keys/merdep.non-prod -o PreferredAuthentications=publickey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $USERNAME@$HOSTNAME "export TEMP_PATH=$TEMP_PATH && python FILE.py -p $TEMP_PATH"
-
-
-
